d100ac97199c50debdbbd0bd16c13dde6148436e
Elena <beklin24@gmail.com>
2025-07-22 10:36:22 +0300

    добавлены алиасы

--- step_3/финальное задание 2.sql
+++ step_3/финальное задание 2.sql
@@ -1,4 +1,3 @@
-
 1.Список уникальных классов. Вывести только названия.
 SELECT DISTINCT class_name
 FROM visits;
@@ -7,19 +6,20 @@ FROM visits;
 SELECT u.user_surname AS surname,
        u.user_name AS name,
        COALESCE(SUM(v.hours_spent), 0) 
-FROM users u
-LEFT JOIN visits v ON u.id_user = v.id_user
-GROUP BY u.id_user, surname, name
+FROM users AS u
+LEFT JOIN visits AS v ON u.id_user = v.id_user
+GROUP BY u.id_user
 ORDER BY surname, name;
 
 3.Средний возраст пользователей, посещающих класс Flex.
 SELECT ROUND(AVG(u.age)) 
-FROM users u
-JOIN visits v ON u.id_user = v.id_user
+FROM users AS u
+LEFT JOIN visits AS v ON u.id_user = v.id_user
 WHERE v.class_name = 'Flex';
 
 4. Вывести пользователей, которые ни разу не посещали бассейн (Swimming pool)
-SELECT users.id_user, users.user_name, users.user_surname, users.user_weight, users.age
-FROM users
-LEFT JOIN visits ON users.id_user = visits.id_user AND visits.class_name = 'Swimming pool'
-WHERE visits.id_visit IS NULL;
+SELECT u.id_user, u.user_name, u.user_surname, u.user_weight, u.age
+FROM users AS u
+LEFT JOIN visits AS v ON u.id_user = v.id_user AND v.class_name = 'Swimming pool'
+WHERE v.id_visit IS NULL;
+
